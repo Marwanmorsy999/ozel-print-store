d6cb6a4bd570b2046bed785973067efd62534dc3
Marwanmorsy999 <m.2396@nu.edu.eg>
2026-06-03 15:37:29 +0300

    fix: restore RootLayout without LanguageProvider

---
+++ src/app/components/layouts/RootLayout.tsx
@@ -0,0 +1,18 @@
+﻿import { Outlet } from 'react-router';
+import { Navigation } from '../Navigation';
+import { Footer } from '../Footer';
+import { CartProvider } from '../../context/CartContext';
+
+export function RootLayout() {
+  return (
+    <CartProvider>
+      <div className="min-h-screen flex flex-col bg-background text-foreground">
+        <Navigation />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </CartProvider>
+  );
+}
