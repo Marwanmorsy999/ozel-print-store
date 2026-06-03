690c810089518ae47d76074280321301f98a04b9
Marwanmorsy999 <m.2396@nu.edu.eg>
2026-06-03 15:18:10 +0300

    fix: move LanguageProvider into RootLayout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,12 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { Toaster } from './components/ui/sonner';
-import { LanguageProvider } from './context/LanguageContext';
 
 export default function App() {
   return (
-    <LanguageProvider>
+    <>
       <RouterProvider router={router} />
       <Toaster position="bottom-right" />
-    </LanguageProvider>
+    </>
   );
 }
--- src/app/components/layouts/RootLayout.tsx
+++ src/app/components/layouts/RootLayout.tsx
@@ -2,17 +2,20 @@ import { Outlet } from 'react-router';
 import { Navigation } from '../Navigation';
 import { Footer } from '../Footer';
 import { CartProvider } from '../../context/CartContext';
+import { LanguageProvider } from '../../context/LanguageContext';
 
 export function RootLayout() {
   return (
-    <CartProvider>
-      <div className="min-h-screen flex flex-col bg-background text-foreground">
-        <Navigation />
-        <main className="flex-1">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </CartProvider>
+    <LanguageProvider>
+      <CartProvider>
+        <div className="min-h-screen flex flex-col bg-background text-foreground">
+          <Navigation />
+          <main className="flex-1">
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </CartProvider>
+    </LanguageProvider>
   );
 }
