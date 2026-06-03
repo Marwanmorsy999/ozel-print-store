d03633046097a097fd71a307aab5dfc99aeaa179
Marwanmorsy999 <m.2396@nu.edu.eg>
2026-06-03 15:25:03 +0300

    fix: RootLayout.tsx content

--- src/app/components/layouts/RootLayout.tsx
+++ src/app/components/layouts/RootLayout.tsx
@@ -1,5 +1,4 @@
-cat > src/app/components/layouts/RootLayout.tsx << 'ENDOFFILE'
-import { Outlet } from 'react-router';
+﻿import { Outlet } from 'react-router';
 import { Navigation } from '../Navigation';
 import { Footer } from '../Footer';
 import { CartProvider } from '../../context/CartContext';
@@ -20,4 +19,3 @@ export function RootLayout() {
     </LanguageProvider>
   );
 }
-ENDOFFILE
